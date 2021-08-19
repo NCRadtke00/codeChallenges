f81340fa78239be77ea965929b09dbd0e3e4e4a8
Nicholas Radtke <Raddnc@gmail.com>
2021-08-19 17:35:04 -0500

    better solution

--- script.js
+++ script.js
@@ -214,15 +214,28 @@
 // })
 // )
 //we'll use closures to create three buttons. Each button will represent a different color and we'll change the background color of the document body to the color value. Using a closure, create a function called change color that accepts a color value. When the button is clicked, the closure should be invoked and the body background color should be updated.
-function makeBodyBlue() {
-  document.body.style.background = "#0f62fe";
+// function makeBodyBlue() {
+//   document.body.style.background = "#0f62fe";
+// }
+// function makeBodyGreen() {
+//   document.body.style.background = "#42be65";
+// }
+// function makeBodyPink() {
+//   document.body.style.background = "#ff7eb6";
+// }
+// document.querySelector(`#blue`).addEventListener("click", makeBodyBlue);
+// document.querySelector(`#green`).addEventListener("click", makeBodyGreen);
+// document.querySelector(`#pink`).addEventListener("click", makeBodyPink);
+function changeColor(color) {
+  document.body.style.background = color;
 }
-function makeBodyGreen() {
-  document.body.style.background = "#42be65";
-}
-function makeBodyPink() {
-  document.body.style.background = "#ff7eb6";
-}
-document.querySelector(`#blue`).addEventListener("click", makeBodyBlue);
-document.querySelector(`#green`).addEventListener("click", makeBodyGreen);
-document.querySelector(`#pink`).addEventListener("click", makeBodyPink);
+
+document
+  .querySelector(`#blue`)
+  .addEventListener("click", () => changeColor("#0f62fe"));
+document
+  .querySelector(`#green`)
+  .addEventListener("click", () => changeColor("#42be65"));
+document
+  .querySelector(`#pink`)
+  .addEventListener("click", () => changeColor("#ff7eb6"));
