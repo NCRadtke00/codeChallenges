092795ece2e78257d84a8f5499b854bc9708360e
Nicholas Radtke <Raddnc@gmail.com>
2021-08-19 00:14:57 -0500

    solved with class solution

--- index.js
+++ index.js
@@ -67,6 +67,24 @@ function Movie(title, director, genre, releaseYear, rating){
 Movie.prototype.getOverview = function(){
     return `${this.title}, a ${this.genre} film  directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
 }
+//class solution
+//class solution
+class Movies{
+    constructor(title, director, genre, releaseYear, rating){
+        this.title = title;
+        this.director =director;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.rating = rating;
+    }
+    get overview(){
+        return this.gerOverview;
+    }
+    getOverview(){
+        return `${this.title}, a ${this.genre} film  directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
+    }
+
+}
 const Spiderman = new Movie("Spiderman", "Sam Raini", "Action", 2002, 87);
 const Batman = new Movie("Batman","Tim Burton", "Action","1989",99)
 console.log(Spiderman);
