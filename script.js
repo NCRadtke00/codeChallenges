65904f9f36e6f6632f82bb79cec7d95ab6b5f1db
Nicholas Radtke <Raddnc@gmail.com>
2021-08-19 17:32:20 -0500

    easy simple no scale solution

--- script.js
+++ script.js
@@ -214,3 +214,15 @@
 // })
 // )
 //we'll use closures to create three buttons. Each button will represent a different color and we'll change the background color of the document body to the color value. Using a closure, create a function called change color that accepts a color value. When the button is clicked, the closure should be invoked and the body background color should be updated.
+function makeBodyBlue() {
+  document.body.style.background = "#0f62fe";
+}
+function makeBodyGreen() {
+  document.body.style.background = "#42be65";
+}
+function makeBodyPink() {
+  document.body.style.background = "#ff7eb6";
+}
+document.querySelector(`#blue`).addEventListener("click", makeBodyBlue);
+document.querySelector(`#green`).addEventListener("click", makeBodyGreen);
+document.querySelector(`#pink`).addEventListener("click", makeBodyPink);
