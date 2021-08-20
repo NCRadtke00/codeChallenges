824678b8a7b6fef32ce75dcd263ebdecc15f06e3
Nicholas Radtke <Raddnc@gmail.com>
2021-08-19 19:44:17 -0500

    created password validator, using regex to check characters

--- script.js
+++ script.js
@@ -333,3 +333,19 @@ getUsers();
 // console.log(urlify("I've got a new job :)"));
 
 //In this challenge, you'll build a password validator for a signup form, which tests whether or not a user's desired password is strong enough. You must use a regular expression in your solution. If the user's password is strong enough, return the string, your password is valid. Otherwise, return the string, your password is invalid. You must also use a ternary operator to return a Boolean value. Ternary operators check to see if a certain condition has been met. We follow the expression we want to check with a question mark, and if the condition is truthy, it will execute the code following the question mark. The truthy expression is followed by a colon. And if the condition is falsy, it will execute the following code. Here are the criteria which must be met in order for a password to be strong enough. The password must have at least one lowercase letter. The password must have at least one uppercase letter. It must contain one digit and one special character. And it must be at least eight characters long.
+function checkPassword(password) {
+  let oneLowerCaseLetter = /(?=.+[a-z])/;
+  let oneUpperCaseLetter = /(?=.+[A-Z])/;
+  let oneDigit = /(?=.+[0-9])/;
+  let oneSpecialCharacter = /(?=.+[!@#$%^&*])/;
+  let minimumEightCharacters = /(?=.{8,})/;
+  let isValid =
+    oneLowerCaseLetter.test(password) &&
+    oneUpperCaseLetter.test(password) &&
+    oneDigit.test(password) &&
+    oneSpecialCharacter.test(password) &&
+    minimumEightCharacters.test(password);
+  return isValid ? "Your password is valid" : "Your password is invalid";
+}
+console.log(checkPassword("abc")); //invalid
+console.log(checkPassword("ShawnSpencer1!")); //valid
