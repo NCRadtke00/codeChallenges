d5ce760fe388ac2ac22fbee59dbae915551ee3a2
Nicholas Radtke <Raddnc@gmail.com>
2021-08-29 23:51:19 -0500

    js challenge for returning highest and lowestnumber from string of spaced separated numbers.

--- script.js
+++ script.js
@@ -418,3 +418,20 @@ function getMaxValue (arrary[]) {
   function addBinary(a,b){
     return (a+b).toString(2)
   }
+
+  //	In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+//highAndLow("1 2 3 4 5");  // return "5 1"
+//highAndLow("1 2 -3 4 5"); // return "5 -3"
+//highAndLow("1 9 3 4 -5"); // return "9 -5"
+//Notes:
+//All numbers are valid Int32, no need to validate them. There will always be at least one number in the input string. Output string must be two numbers separated by a single space, and highest number is first.
+(A1)
+function highAndLow(numbers){
+  numbers = numbers.split('').map(Number);
+  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0,numbers);
+}
+(A2)
+function highAndLow(numbers){
+  numbers = numbers.split('');
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
