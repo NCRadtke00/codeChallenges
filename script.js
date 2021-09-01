121744f8e7b2dc4ced8a12d832d39e6580d15b72
Nicholas Radtke <Raddnc@gmail.com>
2021-08-31 22:11:13 -0500

    solved js challenge by creating a var to count bits from and in put. i started by creating an  array bits, a variable count to record the number of 1's, next i took the array bits and set it eqaul to the input split apart in binary 0's and 1's. then i iterated through the array bits checking for 1 and increasing the count accordingly.

--- script.js
+++ script.js
@@ -507,3 +507,14 @@ function toCamelCase(str){
 
   //	Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
 //Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+var countBits = function(n) {
+  var bits =[];
+  var count = 0;
+  bits = n.toString(2).split('');
+  for (var i=0; i <bits.length;i++){
+    if(bits[i] == 1){
+      count++;
+    }
+  }
+  return count;
+}
