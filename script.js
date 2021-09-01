4506f96628f1bb3a7e42fef494293373143d77a0
Nicholas Radtke <Raddnc@gmail.com>
2021-08-31 23:09:24 -0500

    js challenge to get the sum of the positives and sum of negatives in an array and return it in a new array sum of pos, sum of neg.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //  In this challenge you'll be developing an inventory application for a bookstore. You need to create a book class, which provides information about different books in the store. Each book will have a title, author, ISBN, and keep track of the number of available copies. You'll need a way to get each book's availability. If there aren't any copies of the book left, the function should return out of stock. If there are less than 10 copies, the function should return low stock. Otherwise the function should return in stock. You'll also need a function for selling a book. This will take the number of copies sold and subtract it from the total number of copies. If no argument is passed, we can default the number of copies to sell to one. Lastly, you'll want a restock function, which takes in a number of copies to restock and adds it to the total number of copies. If no argument is passed, we can default the restock number to five. You should use JavaScript's class keyword as well as a getter function for the availability method.
 
 const { I } = require("caniuse-lite/data/agents");
+const { x } = require("caniuse-lite/dist/lib/supported");
 
 // // function solution
 // function (title, author, ISBN, numCopies){
@@ -541,3 +542,10 @@ function alphabetPosition(text) {
   return text.toUpperCase().match(/[a-z]/gi).map((c) => c.charCodeAt() - 64).join(' ');
 }
 console.log(alphabetPosition("Hi, I'm Nick!"));// HYB
+
+
+//Given an array of integers. Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. If the input array is empty or null, return an empty array:
+
+function countPositivesSumNegatives(input) {
+  return;
+}
