7e9e5e9cb964f65ff671de6828a86119e5b4a74d
Nicholas Radtke <Raddnc@gmail.com>
2021-08-31 21:48:38 -0500

    js challenge to take an int n, and return the number of bits that are equal to one in the binaray rep of n

--- script.js
+++ script.js
@@ -503,3 +503,7 @@ function toCamelCase(str){
     const matches = arr.join('').match(/[;:][-~]?[(D)]/g);
     return matches ? matches.length : 0;
   }
+
+
+  //	Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+//Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
