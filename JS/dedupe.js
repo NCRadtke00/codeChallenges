b71f7ffb115b160d94587fb291134cebac30fbda
RadtkeN <raddnc@gmail.com>
2022-05-24 15:39:20 -0500

    fixed check, set loop, checking if value is not equal to next value, setting value, adding nonunique values

--- JS/dedupe.js
+++ JS/dedupe.js
@@ -4,10 +4,20 @@ var expectedArr = [0, 1, 2, 3, 4, 2, 2, 3, 3, 4]; // The expected answer with co
 var k = removeDuplicates(arr);
 Console.WriteLine(string.Join(",", arr));
 function removeDuplicates(arr) {
-    if (arr.length = 0 || arr.length = 1) {
+    if (arr.length == 0 || arr.length == 1) {
         return arr.length;
     }
     // checking if the arr is empty or with 1 element
     var j = 0;
+    //to store index of next unique element
+    for (var i = 0; i < arr.length - 1; i++) {
 
+        if (arr[i] != arr[i + 1]) {
+            //check if i is not equal to next value
+            arr[j++] = arr[i];
+            //store value if i is unique
+        }
+        arr[j++] = arr[arr.length - 1];
+    }
+    return j;
 };
