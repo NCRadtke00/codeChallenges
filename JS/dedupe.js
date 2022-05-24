428b28a1e5c679848054c4172fd9ffd7ed6527e9
RadtkeN <raddnc@gmail.com>
2022-05-24 15:28:35 -0500

    solving dedupe in JS

---
+++ JS/dedupe.js
@@ -0,0 +1,9 @@
+var arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]; // Sorted Input array
+var expectedArr = [0, 1, 2, 3, 4, 2, 2, 3, 3, 4]; // The expected answer with correct length
+
+var k = removeDuplicates(arr);
+Console.WriteLine(string.Join(",", arr));
+function removeDuplicates(arr) {
+
+
+};
